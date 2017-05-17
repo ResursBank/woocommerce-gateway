61e82f8e1941e93336e011b2f04551676b7f6d8a
Tomas Tornevall <tomas.tornevall@resurs.se>
2017-05-17 16:38:24 +0200

    Resurs Checkout for Safari browsers (critical patch)

--- js/omnicheckout.js
+++ js/omnicheckout.js
@@ -1,4 +1,4 @@
-/*! ResursCheckoutJS v0.07 - Generic Reurs Bank iFrame-driver for Resurs Checkout, for catching events in the Resurs Checkout iFrame */
+/*! ResursCheckoutJS v0.05 - Generic Reurs Bank iFrame-driver for Resurs Checkout, for catching events in the Resurs Checkout iFrame */
 
 /*
  * CHANGELOG
@@ -11,7 +11,7 @@
  *
  * Requirements:
  *
- *      - RESURSCHECKOUT_IFRAME_URL (OMNICHECKOUT_IFRAME_URL) needs to be set from your store, to define where events are sent from.
+ *      - RESURSCHECKOUT_IFRAME_URL needs to be set from your store, to define where events are sent from.
  *      - Make sure that shopUrl is sent and matches with the target iFrame URL, when creating the iFrame at API level.
  *      - A html element that holds the iframe
  *
@@ -22,12 +22,12 @@
  * The script is written so that you can put it on a webpage without having it primarily activated (to avoid colliding with other scripts).
  * It utilizes the message handler in the Resurs Checkout iframe, so that you can push an order into the store in the background, as the checkout is completed at Resurs Bank.
  *
- * Events that this module catches from Resurs Checkout:
+ * Events to catch from Resurs Checkout:
  *
- *      checkout:loaded                 - Handled by this script when the iFrame has loaded and is ready
- *      checkout:user-info-change       - Stored until checkout is finished (Data passed to function resursCheckoutCustomerChange if set)
- *      checkout:payment-method-change  - Stored until checkout is finished
- *      checkout:booking-order          - Passed with necessary customer data to a callback, or dropped if no callback is set
+ *      omnicheckout:loaded                 - Handled by this script when the iFrame has loaded and is ready
+ *      omnicheckout:user-info-change       - Stored until checkout is finished
+ *      omnicheckout:payment-method-change  - Stored until checkout is finished
+ *      omnicheckout:booking-order          - Passed with necessary customer data to a callback, or dropped if no callback is set
  *
  *      As Resurs Checkout will rename events in short, please take a look at the variable currentResursEventNamePrefix, if you need to make a quickfix.
  *
@@ -50,51 +50,23 @@
  */
 
 if (typeof ResursCheckout !== "function" && typeof ResursCheckout === "undefined") {
-    var currentResursEventNamePrefix = "checkout";
-
     function ResursCheckout() {
-        var resursCheckoutElement = "";     // defined element for where Resurs Checkout iframe is (or should be) located
+        var currentResursEventNamePrefix = "omnicheckout";
+        var resursCheckoutElement = "";
         var resursCheckoutFrame = "";
-        var resursCheckoutVersion = "0.06";
+        var resursCheckoutVersion = "0.05";
         var resursCheckoutData = {"paymentMethod": "", "customerData": {}};
         var resursCheckoutDebug = false;
         var resursCheckoutBookingCallback = null;
         var resursCheckoutPurchaseFail = null;
-        var resursCheckoutCustomerChange = null;
-        var resursPostMessageElement = null;
-        // If there is an argument, there is something else implemented as the Resurs Checkout-container than the default.
-        if (typeof arguments[0] !== "undefined") {
-            resursCheckoutElement = arguments[0];   // Accepting user defined elements
-        } else {
-            resursCheckoutElement = "resurs-checkout-container";    // If there are no user defined element, fall back to default
-        }
-        // Backwarding to wider browser compatibility as developers might have already been starting to use #elementIdNames instead of elementIdNames
-        if (resursCheckoutElement.substr(0, 1) === "#") {
-            resursCheckoutElement = resursCheckoutElement.substr(1);
-        }
-
         /*
-         * Developer note: querySelector can do the same here, but has limited compatibility down to IE8. As we try to be
-         * as browser friendly as possible, we also try to use as browser wide functions as possible.
+         * If there is an argument, there is something else implemented as the Resurs Checkout-container than the default.
          */
-
-        // Find out whether our specified element exists or not and, if not, try to fall back to the prior name
-        if (null === document.getElementById(resursCheckoutElement) && null !== document.getElementById('omni-checkout-container')) {
-            resursCheckoutElement = "omni-checkout-container";
-            if (resursCheckoutDebug) {
-                console.log("ResursCheckoutJS: [Config] Former element of Resurs Checkout is present");
-            }
+        if (typeof arguments[0] !== "undefined") {
+            resursCheckoutElement = arguments[0];
+        } else {
+            resursCheckoutElement = "#resurs-checkout-container";
         }
-
-        // We should now have an element defined where an iframe for Resurs Bank should reside. This is where we prepare that element for
-        // the postmessaging.
-        if (null !== document.getElementById(resursCheckoutElement)) {
-            resursPostMessageElement = document.getElementById(resursCheckoutElement);
-            if (resursCheckoutDebug) {
-                console.log("ResursCheckoutJS: [Config] iframe container is set to " + resursCheckoutElement);
-            }
-        }
-
         var resursCheckoutDomain = "";
         /*
          * If RESURSCHECKOUT_IFRAME_URL (RESURSCHECKOUT_IFRAME_URL for compatibility) is set, the script will know where the communication will be. Without this, there may be problems.
@@ -102,37 +74,23 @@ if (typeof ResursCheckout !== "function" && typeof ResursCheckout === "undefined
         if (typeof RESURSCHECKOUT_IFRAME_URL !== "undefined") {
             resursCheckoutDomain = RESURSCHECKOUT_IFRAME_URL;
         }
-        if (typeof OMNICHECKOUT_IFRAME_URL !== "undefined") {
-            resursCheckoutDomain = OMNICHECKOUT_IFRAME_URL;
+        if (typeof RESURSCHECKOUT_IFRAME_URL !== "undefined") {
+            resursCheckoutDomain = RESURSCHECKOUT_IFRAME_URL;
         }
-        /**
-         * iFrame speech function.
-         * @param data
-         */
+        var function_exists = function (functionName) {
+            if (typeof functionName === "function") {
+                return true;
+            }
+        };
         var postMessage = function (data) {
             var resursCheckoutWindow;
-            if (null !== resursPostMessageElement) {
-                if (resursCheckoutDebug) {
-                    console.log("ResursCheckoutJS: [PrePostMessage] Using resursPostMessageElement as destination");
-                }
-                resursCheckoutFrame = resursPostMessageElement.getElementsByTagName("iframe")[0];
-                resursCheckoutWindow = resursCheckoutFrame.contentWindow || resursCheckoutFrame.contentDocument;
-            } else {
-                if (resursCheckoutDebug) {
-                    console.log("ResursCheckoutJS: [PrePostMessage] Locating postMessage-element as resursPostMessageElement is missing");
-                }
-                resursCheckoutFrame = document.getElementsByTagName("iframe")[0];
-                resursCheckoutWindow = resursCheckoutFrame.contentWindow || resursCheckoutFrame.contentDocument;
-            }
-            if (resursCheckoutWindow && typeof resursCheckoutWindow.postMessage === "function" && typeof resursCheckoutFrame.src !== "undefined" && typeof resursCheckoutDomain === 'string' && resursCheckoutDomain !== '' && resursCheckoutFrame.src.indexOf(resursCheckoutDomain) > -1) {
-                if (resursCheckoutDebug) {
-                    console.log("ResursCheckoutJS: [postMessage] " + JSON.stringify(data));
-                }
+            /*
+             * Find the current active iframe
+             */
+            resursCheckoutFrame = document.getElementsByTagName("iframe")[0];
+            resursCheckoutWindow = resursCheckoutFrame.contentWindow || resursCheckoutFrame.contentDocument;
+            if (resursCheckoutWindow && typeof resursCheckoutDomain === 'string' && resursCheckoutDomain !== '') {
                 resursCheckoutWindow.postMessage(JSON.stringify(data), resursCheckoutDomain);
-            } else {
-                if (resursCheckoutDebug) {
-                    console.log("ResursCheckoutJS: [postMessage] iframe window missing postMessage function");
-                }
             }
         };
         var ResursCheckout = {
@@ -146,22 +104,19 @@ if (typeof ResursCheckout !== "function" && typeof ResursCheckout === "undefined
             setPurchaseFailCallback: function (eventCallbackSet) {
                 resursCheckoutPurchaseFail = eventCallbackSet;
             },
-            setCustomerChangedEventCallback: function (eventCallbackSet) {
-                resursCheckoutCustomerChange = eventCallbackSet;
-            },
             setDebug: function (activateDebug) {
                 if (activateDebug == 1) {
                     activateDebug = true;
                 }
                 resursCheckoutDebug = activateDebug;
-                console.log("ResursCheckoutJS: [Config] Verbosity level raised");
+                console.log("ResursCheckoutJS verbosity level raised");
             },
             confirmOrder: function (successfulness) {
                 if (typeof successfulness !== "boolean") {
                     successfulness = false;
                 }
                 if (resursCheckoutDebug) {
-                    console.log("ResursCheckoutJS: [Outbound] Confirm order with boolean: " + successfulness);
+                    console.log("ResursCheckoutJS - Confirm order with boolean: " + successfulness);
                 }
                 postMessage({
                     eventType: currentResursEventNamePrefix + ":order-status",
@@ -171,7 +126,7 @@ if (typeof ResursCheckout !== "function" && typeof ResursCheckout === "undefined
             postFrame: function (postMessageData) {
                 if (typeof postMessageData === "object") {
                     if (resursCheckoutDebug) {
-                        console.log("ResursCheckoutJS: [Outbound] Sending message to iframe: " + JSON.stringify(postMessageData));
+                        console.log("ResursCheckoutJS - Sending message to iframe: " + JSON.stringify(postMessageData));
                     }
                     postMessage(postMessageData);
                 }
@@ -207,12 +162,12 @@ if (typeof ResursCheckout !== "function" && typeof ResursCheckout === "undefined
                                 if (eventDataObject.eventType.indexOf(currentResursEventNamePrefix) > -1) {
                                     if (eventDataObject.eventType == currentResursEventNamePrefix + ":payment-method-change") {
                                         if (resursCheckoutDebug) {
-                                            console.log("ResursCheckoutJS: [Inbound] payment-method-change");
+                                            console.log("ResursCheckoutJS - payment-method-change");
                                         }
                                         resursCheckoutData.paymentMethod = eventDataObject.method;
                                     } else if (eventDataObject.eventType == currentResursEventNamePrefix + ":user-info-change") {
                                         if (resursCheckoutDebug) {
-                                            console.log("ResursCheckoutJS: [Inbound] user-info-change");
+                                            console.log("ResursCheckoutJS - user-info-change");
                                         }
                                         resursCheckoutData.customerData = {
                                             "address": (typeof eventDataObject.address !== "undefined" ? eventDataObject.address : {}),
@@ -220,9 +175,6 @@ if (typeof ResursCheckout !== "function" && typeof ResursCheckout === "undefined
                                             "ssn": eventDataObject.ssn,
                                             "paymentMethod": (typeof resursCheckoutData.paymentMethod !== "undefined" ? resursCheckoutData.paymentMethod : "")
                                         };
-                                        if (typeof resursCheckoutCustomerChange === "function") {
-                                            resursCheckoutCustomerChange(resursCheckoutData);
-                                        }
                                     } else if (eventDataObject.eventType == currentResursEventNamePrefix + ":purchase-failed") {
                                         if (typeof resursCheckoutPurchaseFail === "function") {
                                             resursCheckoutPurchaseFail();
@@ -237,7 +189,7 @@ if (typeof ResursCheckout !== "function" && typeof ResursCheckout === "undefined
                                          */
                                         if (typeof resursCheckoutBookingCallback === "function") {
                                             if (resursCheckoutDebug) {
-                                                console.log("ResursCheckoutJS: [Outbound] puchase-button-clicked event received, bounce back through user defined callback.");
+                                                console.log("ResursCheckoutJS - puchase-button-clicked event received, user defined callback is used.");
                                             }
                                             resursCheckoutBookingCallback(resursCheckoutData, eventDataObject);
                                         } else if (typeof resursCheckoutBookingCallback === "string") {
@@ -250,7 +202,7 @@ if (typeof ResursCheckout !== "function" && typeof ResursCheckout === "undefined
                                              * in the shop. In that case, we'll just continue with the booking part.
                                              */
                                             if (resursCheckoutDebug) {
-                                                console.log("ResursCheckoutJS: [Outbound] puchase-button-clicked event received and no user defined callbacks was found. Cosidering autoConfirm.");
+                                                console.log("ResursCheckoutJS - puchase-button-clicked event received and not callbacks was found. Automatically confirming.");
                                             }
                                             postMessage({
                                                 eventType: currentResursEventNamePrefix + ":order-status",
