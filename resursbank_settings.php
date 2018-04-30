2ab0db5dd40934f06d2692f58e2245e4921fc6eb
Tomas Tornevall <tomas.tornevall@resurs.se>
2018-04-30 14:50:40 +0200

    Plugininfo added

--- resursbank_settings.php
+++ resursbank_settings.php
@@ -448,25 +448,48 @@ class WC_Settings_Tab_ResursBank extends WC_Settings_Page {
 	 *
 	 * @return mixed|null
 	 */
-	function getDefined($definedConstantName = '') {
-	    if (defined($definedConstantName)) {
-	        return constant($definedConstantName);
-        }
-        return null;
-    }
+	function getDefined( $definedConstantName = '' ) {
+		if ( defined( $definedConstantName ) ) {
+			return constant( $definedConstantName );
+		}
+
+		return null;
+	}
 
 	/**
 	 * @return string
 	 */
 	function getPluginInformation() {
+
+	    $hasSoap = class_exists("\SoapClient") ? true : false;
+	    $hasCurlInit = true;
+	    $hasSsl = true;
+		if ( ! function_exists( 'curl_init' ) ) {
+			$hasCurlInit = false;
+		}
+		$streamWrappers = @stream_get_wrappers();
+        if (!in_array('https', $streamWrappers)) {
+            $hasSsl = false;
+        }
+
 		$pluginInfo = $this->setSeparator( __( 'Plugin information', 'WC_Payment_Gateway' ) );
 		$pluginInfo .= '<tr><td>Plugin/Gateway</td><td>v' . rbWcGwVersion() . '</td></tr>';
 		$pluginInfo .= '<tr><td>PHP</td><td>' . ( defined( 'PHP_VERSION' ) ? "v" . PHP_VERSION : "" ) . '</td></tr>';
 		$pluginInfo .= '<tr><td>EComPHP</td><td>' . $this->flow->getVersionFull() . '</td></tr>';
-		$pluginInfo .= '<tr><td>CURL MODULES</td><td> NETCURL-v' . $this->getDefined( 'NETCURL_RELEASE' ) . ' MODULE_CURL-v' . $this->getDefined( 'NETCURL_CURL_RELEASE' ) . '</td></tr>';
+		$pluginInfo .= '<tr><td>Communications library</td><td> NETCURL-v' . $this->getDefined( 'NETCURL_RELEASE' ) . ' / MODULE_CURL-v' . $this->getDefined( 'NETCURL_CURL_RELEASE' ) . ' / MODULE_SOAP-v' . $this->getDefined( 'NETCURL_SIMPLESOAP_RELEASE' ) . '-' . $this->getDefined( 'NETCURL_SIMPLESOAP_MODIFY' ) . '</td></tr>';
+		$pluginInfo .= '<tr><td>curl driver</td><td>' . $this->displayAvail($hasCurlInit) . '</td></tr>';
+		$pluginInfo .= '<tr><td>SoapClient</td><td>' . $this->displayAvail($hasSoap) . '</td></tr>';
+		$pluginInfo .= '<tr><td>SSL/https (wrapper)</td><td>' . $this->displayAvail($hasSsl) . '</td></tr>';
 
 		return $pluginInfo;
 	}
+
+	function displayAvail($boolValue) {
+	    if ($boolValue == true) {
+	        return '<div style="color:#009900; font-weight: bold;">' . __('Available', 'WC_Payment_Gateway') . '</div>';
+        }
+		return '<div style="color:#990000; font-weight: bold;">' . __('Not available', 'WC_Payment_Gateway') . '</div>';
+    }
 
 	/**
 	 * Primary configuration tab
