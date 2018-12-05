f89908b2bb554f7c090228b6e81c6e5f8e20ef61
Tomas Tornevall <tomas.tornevall@resurs.se>
2018-12-05 14:01:48 +0100

    Cleanup.

--- js/resursadmin.js
+++ js/resursadmin.js
@@ -12,7 +12,6 @@ function getResursBankSelectField(fieldName) {
 
 function resursBankCredentialField() {
     var cId = parseInt(Math.random() * 1000);
-
     var row = $resurs_bank('<tr>');
     row.append($resurs_bank('<td>').html(
         getResursBankInputField('resursbank_credentials[' + cId + '][username]', 'Username')).prepend('<b>Username</b><br>')
@@ -23,19 +22,5 @@ function resursBankCredentialField() {
     row.append($resurs_bank('<td>').html(
         getResursBankSelectField('resursbank_credentials[' + cId + '][country]', 'Country')).prepend('<b>Country</b><br>')
     );
-
     $resurs_bank('#resurs_bank_credential_table').append(row);
-
-    /*    var tr = document.createElement('tr');
-        var htmlCredentialFields = '<td><b>Username</b><br><input name="resursbank_credentials[' + cId + '][username]" value=""></td>' +
-            '<td><b>Password</b><br><input name="resursbank_credentials[' + cId + '][password]" value=""></td>' +
-            '<td><b>Country</b><br><select name="resursbank_credentials[' + cId + '][country]">' +
-            '<option value="SE">Sverige</option>' +
-            '<option value="DK">Danmark</option>' +
-            '<option value="NO">Norge</option>' +
-            '<option value="FI">Suomi</option>' +
-            '</select>' +
-            '</td>';
-        tr.innerHTML = htmlCredentialFields;
-        document.getElementById('resurs_bank_credential_table').appendChild(tr);*/
 }
