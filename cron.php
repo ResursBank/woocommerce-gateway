26c2b644527b97d9ba8f1f9f40547e47cb27e8bf
Tomas Tornevall <tomas.tornevall@resurs.se>
2018-12-17 13:04:10 +0100

    Cron file experimental

--- cron.php
+++ cron.php
@@ -1,2 +1,26 @@
 <?php
 
+// Experimental, do not use
+die;
+
+define('RESURS_BANK_CRON', true);
+
+/** @var string $startPath */
+$startPath = isset($argv[1]) ? $argv[1] : null;
+
+/** @var string $resursPath */
+$resursPath = isset($argv[2]) ? $argv[2] : null;
+
+$startFile = $startPath . '/wp-blog-header.php';
+
+if (!empty($startPath) && file_exists($startFile) && file_exists($resursPath . '/init.php')) {
+    define('WP_USE_THEMES', false);
+
+    /** @noinspection PhpIncludeInspection */
+    require_once($startFile);
+
+    /** @noinspection PhpIncludeInspection */
+    require_once($resursPath);
+
+    //echo Resursbank_Core::get_payment_methods('', true);
+}
