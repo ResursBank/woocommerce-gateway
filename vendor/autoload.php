370d7e7e77ae34c42779e4a4cef65320cfb6362c
Tomas Tornevall <tomas.tornevall@resurs.se>
2018-12-18 07:26:48 +0100

    packages updated

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit28ea1bda4852cb0e4d4b50a4a29e34c2::getLoader();
+return ComposerAutoloaderInitfe7df848ae650aebcb8fbde5da6c2c35::getLoader();
