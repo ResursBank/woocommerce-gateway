15db64b78a27d6d6e006df82f03ba535110978d9
Tomas Tornevall <thorne@tornevall.net>
2019-08-23 09:42:34 +0200

    Version data tagged.

--- vendor/resursbank/ecomphp/source/classes/rbapiloader.php
+++ vendor/resursbank/ecomphp/source/classes/rbapiloader.php
@@ -8,7 +8,7 @@
  * @author  Resurs Bank Ecommerce
  *          /home/thorne/dev/Resurs/ecomphp/1.1/source/classes/rbapiloader.php<ecommerce.support@resurs.se>
  * @branch  1.3
- * @version 1.3.20
+ * @version 1.3.21
  * @link    https://test.resurs.com/docs/x/KYM0 Get started - PHP Section
  * @link    https://test.resurs.com/docs/x/TYNM EComPHP Usage
  * @link    https://test.resurs.com/docs/x/KAH1 EComPHP: Bitmasking features
@@ -59,10 +59,10 @@ use TorneLIB\NETCURL_POST_DATATYPES;
 
 // Globals starts here
 if (!defined('ECOMPHP_VERSION')) {
-    define('ECOMPHP_VERSION', '1.3.19');
+    define('ECOMPHP_VERSION', '1.3.21');
 }
 if (!defined('ECOMPHP_MODIFY_DATE')) {
-    define('ECOMPHP_MODIFY_DATE', '20190527');
+    define('ECOMPHP_MODIFY_DATE', '20190823');
 }
 
 /**
