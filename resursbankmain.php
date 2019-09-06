883c349964f377bf7aea1e1913a86e2ec04144ab
Tomas Tornevall <thorne@tornevall.net>
2019-09-06 11:19:20 +0200

    Adds support to change "Skapad av" in payment admin. For woo, it will look like: Skapad av: WOO/supervisor

--- resursbankmain.php
+++ resursbankmain.php
@@ -5626,6 +5626,7 @@ function initializeResursFlow(
     /** @var $initFlow \Resursbank\RBEcomPHP\ResursBank */
     $initFlow = new \Resursbank\RBEcomPHP\ResursBank($username, $password);
     $initFlow->setSimplifiedPsp(true);
+    $initFlow->setRealClientName('WOO');
 
     if (isResursHosted()) {
         $initFlow->setPreferredPaymentFlowService(RESURS_FLOW_TYPES::FLOW_HOSTED_FLOW);
