687e02ddf4cdc0f8b8eeaf70c4c0f78ee0a18f02
Tomas Tornevall <thorne@tornevall.net>
2020-06-10 15:47:32 +0200

    WOO-479: rcojs-facelift
    Splitting up framework usages for rcojs.

---
+++ js/rcoface.js
@@ -0,0 +1,6 @@
+var $RB = jQuery.noConflict();
+
+$RB(document).ready(function ($) {
+    if (typeof window.RCO !== 'undefined') {
+    }
+});
--- resursbankmain.php
+++ resursbankmain.php
@@ -4640,8 +4640,20 @@ function woocommerce_gateway_resurs_bank_init()
             RB_WOO_VERSION . (defined('RB_ALWAYS_RELOAD_JS') && RB_ALWAYS_RELOAD_JS === true ? "-" . time() : "")
         );
         wp_enqueue_script(
+            'rcoface',
+            plugin_dir_url(__FILE__) . 'js/rcoface.js' . $oneRandomValue,
+            ['jquery'],
+            RB_WOO_VERSION . (defined('RB_ALWAYS_RELOAD_JS') && RB_ALWAYS_RELOAD_JS === true ? "-" . time() : "")
+        );
+        wp_enqueue_script(
             'resursbankmain',
             plugin_dir_url(__FILE__) . 'js/resursbank.js' . $oneRandomValue,
+            ['jquery'],
+            RB_WOO_VERSION . (defined('RB_ALWAYS_RELOAD_JS') && RB_ALWAYS_RELOAD_JS === true ? "-" . time() : "")
+        );
+        wp_enqueue_script(
+            'rcojs',
+            plugin_dir_url(__FILE__) . 'js/rcojs.js' . $oneRandomValue,
             ['jquery'],
             RB_WOO_VERSION . (defined('RB_ALWAYS_RELOAD_JS') && RB_ALWAYS_RELOAD_JS === true ? "-" . time() : "")
         );
