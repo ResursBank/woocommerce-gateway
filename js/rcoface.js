b9f3d6096cc2e7efe74c92ef6c56b2a338174c24
Tomas Tornevall <thorne@tornevall.net>
2020-06-11 11:12:54 +0200

    faceliftjs killswitch patch

--- js/rcoface.js
+++ js/rcoface.js
@@ -1,6 +1,8 @@
 var $RB = jQuery.noConflict();
 
+var faceliftActive = false;
 $RB(document).ready(function ($) {
     if (typeof window.RCO !== 'undefined') {
+        //faceliftActive = true;
     }
 });
