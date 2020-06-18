ee9fcc31c103a558c7c192be79f564b621256a27
Tomas Tornevall <thorne@tornevall.net>
2020-06-18 11:10:44 +0200

    ip addr inspection

--- resursbankmain.php
+++ resursbankmain.php
@@ -3620,11 +3620,13 @@ function woocommerce_gateway_resurs_bank_init()
          */
         public static function get_ip_address()
         {
+            $return = $_SERVER['REMOTE_ADDR'];
+
             $handleNatConnections = getResursOption('handleNatConnections');
             if ($handleNatConnections) {
                 // check for shared internet/ISP IP
                 if (!empty($_SERVER['HTTP_CLIENT_IP']) && self::validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
-                    return $_SERVER['HTTP_CLIENT_IP'];
+                    $return = $_SERVER['HTTP_CLIENT_IP'];
                 }
                 // check for IPs passing through proxies
                 if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
@@ -3632,26 +3634,27 @@ function woocommerce_gateway_resurs_bank_init()
                     $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
                     foreach ($iplist as $ip) {
                         if (self::validate_ip($ip)) {
-                            return $ip;
+                            $return = $ip;
+                            break;
                         }
                     }
                 }
                 if (!empty($_SERVER['HTTP_X_FORWARDED']) && self::validate_ip($_SERVER['HTTP_X_FORWARDED'])) {
-                    return $_SERVER['HTTP_X_FORWARDED'];
+                    $return = $_SERVER['HTTP_X_FORWARDED'];
                 }
                 if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && self::validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
-                    return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+                    $return = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
                 }
                 if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && self::validate_ip($_SERVER['HTTP_FORWARDED_FOR'])) {
-                    return $_SERVER['HTTP_FORWARDED_FOR'];
+                    $return = $_SERVER['HTTP_FORWARDED_FOR'];
                 }
                 if (!empty($_SERVER['HTTP_FORWARDED']) && self::validate_ip($_SERVER['HTTP_FORWARDED'])) {
-                    return $_SERVER['HTTP_FORWARDED'];
+                    $return = $_SERVER['HTTP_FORWARDED'];
                 }
             }
 
             // return unreliable ip since all else failed
-            return $_SERVER['REMOTE_ADDR'];
+            return $return;
         }
 
         /** @var string $ip Access to undeclared static property fix */
