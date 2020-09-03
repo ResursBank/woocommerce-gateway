37d3c70e4cb2b23ed3ffaf82472f666747bdff3a
Tomas Tornevall <thorne@tornevall.net>
2020-09-03 13:55:27 +0200

    tmp test

--- js/resursbank.js
+++ js/resursbank.js
@@ -229,6 +229,9 @@ $RB(document).ready(function ($) {
                      * radio buttons is naturally removed from the site. In THAT case, we should check if both radion
                      * buttons is missing and proceed with getAddress if that is the case.
                      */
+
+                    console.log(currentCustomerType);
+                    console.log($RB('#ssnCustomerType' + currentCustomerType.toUpperCase()).length);
                     if (($RB('#ssnCustomerType' + currentCustomerType.toUpperCase()).length > 0 &&
                             $RB('input[id^="payment_method_resurs_bank"]').length > 0
                         ) ||
